89c6a3dffe66eb9b5abbc090bf390c64c4a5db59
cesar rodriguez <cesar.rodriguez@unosquare.com>
2024-02-16 12:25:11 -0600

    adding test for get, put and post request methods

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -1,3 +1,5 @@
+using RestSharp;
+
 namespace ApiTestingDemo;
 
 public class Tests
@@ -5,11 +7,85 @@ public class Tests
     [SetUp]
     public void Setup()
     {
+        Console.WriteLine("--- Request Started ---");
     }
 
     [Test]
-    public void Test1()
+    public void TestSingleUser()
     {
-        Assert.Pass();
+        var client = new RestClient("https://reqres.in/");
+        var request = new RestRequest("/api/users/2");
+        var response = client.Get(request);
+
+        Console.WriteLine($"Body GET METHOD: {response.Content}");
+
+        int reponseStatusCode = (int)response.StatusCode;
+        Assert.That(reponseStatusCode == 200);
+        Assert.IsNotNull(response.Content);
+    }
+
+    [Test]
+    public void TestUpdate()
+    {
+        var client = new RestClient("https://reqres.in/");
+        var request = new RestRequest("/api/users/2");
+        request.AddBody(new
+        {
+            name = "morpheus",
+            job = "zion resident"
+        });
+        var response = client.Put(request);
+
+        Console.WriteLine($"Body PUT METHOD: {response.Content}");
+
+        int reponseStatusCode = (int)response.StatusCode;
+        Assert.That(reponseStatusCode == 200);
+        Assert.IsNotNull(response.Content);
+    }
+
+    [Test]
+    public void TestRegisterSuccessful()
+    {
+        var client = new RestClient("https://reqres.in/");
+        var request = new RestRequest("/api/register");
+        request.AddBody(new
+        {
+            email = "eve.holt@reqres.in",
+            password = "pistol"
+        });
+        var response = client.Post(request);
+
+        Console.WriteLine($"Body POST METHOD: {response.Content}");
+        Console.WriteLine("Register Successful");
+
+        int reponseStatusCode = (int)response.StatusCode;
+        Assert.That(reponseStatusCode == 200);
+        Assert.IsNotNull(response.Content);
+    }
+
+    [Test]
+    public void TestLoginSuccessful()
+    {
+        var client = new RestClient("https://reqres.in/");
+        var request = new RestRequest("/api/login");
+        request.AddBody(new
+        {
+            email = "eve.holt@reqres.in",
+            password = "cityslicka"
+        });
+        var response = client.Post(request);
+
+        Console.WriteLine($"Body POST METHOD: {response.Content}");
+        Console.WriteLine("Login Successful");
+
+        int reponseStatusCode = (int)response.StatusCode;
+        Assert.That(reponseStatusCode == 200);
+        Assert.IsNotNull(response.Content);
+    }
+
+    [TearDown]
+    public void TearDown()
+    {
+        Console.WriteLine("--- Request Finished ---");
     }
 }
